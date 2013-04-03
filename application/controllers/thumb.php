c2c92105f83a8c04572f74b057ac4146e12e266d
Yan Jiang <yjiang@eee168.com>
2013-04-03 14:10:59 +0800

    完成缩略图

--- application/controllers/thumb.php
+++ application/controllers/thumb.php
@@ -1,10 +1,28 @@
 <?php
-class thumb {
+
+require_once BASEPATH . '/third_party/phpthumb/ThumbLib.inc.php';
+
+class thumb extends MY_Controller{
 	public function __construct(){
-		$this->load->library('thumb');
+		parent::__construct();
 	}
 
 	public function index(){
-		echo $this->PicThumb(); 
+		$url = $this->input->get('url');
+		try{
+			$thumb = PhpThumbFactory::create($url);  
+		}
+		catch (Exception $e){
+			print $e->getMessage();
+		}
+		$size = $this->input->get('size');
+		if($size) {
+			list($width, $height) = explode('x', $size);
+			$thumb->resize($width, $height);
+		}
+		else {
+			$thumb->resizePercent(100);
+		}
+		$thumb->show();
 	}
 }
